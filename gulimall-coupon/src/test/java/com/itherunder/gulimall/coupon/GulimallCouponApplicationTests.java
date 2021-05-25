946e2e93ce1cab1936fd55591ef9b46b96c34590
itherunder <liaozhou98@qq.com>
2021-05-25 15:48:15 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/itherunder/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itherunder.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
